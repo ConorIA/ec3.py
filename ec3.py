20bcd9d46a2c7227ff7fcae0e7d24338232e543a
Conor Anderson <conor@conr.ca>
2019-01-26 21:26:46 -0500

    Major update to improve search feature.
    Squashed commit of the following:
    
    commit bf5930cc842bd763abc51b9fdbd0e397fdafb731
    Author: Conor Anderson <conor@conr.ca>
    Date:   Sat Jan 26 21:26:29 2019 -0500
    
        Avoid empty dirname output.
    
    commit d32c81493298e8a87ff4d8084e2748ffb6de7f67
    Author: Conor Anderson <conor@conr.ca>
    Date:   Sat Jan 26 21:20:18 2019 -0500
    
        Specify numpy version to workaround bug
    
    commit 6b4c75b397216f2cf3969524fceaf61208af991e
    Author: Conor Anderson <conor@conr.ca>
    Date:   Sat Jan 26 21:03:29 2019 -0500
    
        Add numpy to depends
    
    commit 8b7e024cfcdd6bc5b7985b07534eb7a0e0ec8de6
    Author: Conor Anderson <conor@conr.ca>
    Date:   Sat Jan 26 20:58:05 2019 -0500
    
        Port over all of the search stuff
    
    commit e6a640a542b377300ac92b8e379ce664dc8c6247
    Author: Conor Anderson <conor@conr.ca>
    Date:   Sat Jan 26 17:06:45 2019 -0500
    
        Search WIP

--- ec3.py
+++ ec3.py
@@ -1,55 +1,189 @@
 """
 Usage:
-  ec3 (-i | -s <station>...) [options]
+  ec3 inv
+  ec3 find [--name <name>] [--prov <province>...] [(--period <period> --type <type>)] [--recodes] [(--target <y> [<x>] [--dist <distance>])] [--outfile <filename>]
+  ec3 get -s <station>... [options] [--outfile <filename>]
   ec3 (-h | --help)
+  ec3 --version
 
 Facilitates download of hourly, daily, or monthly climate data from Environment and Climate Change Canada
 
-Download the data inventory:
-    -i             Download the inventory of available station data and exit
+Commands:
+  inv                  Download the inventory of available station data and exit
+  find                 Search through the inventory for available data (see "Search Options", below)
+  get                  Download data (see "Download Options", below)
 
-Downloading station data:
-    -s <station>   Station code to download. Pass the argument multiple times for more than one
-                   station: e.g. -s 5051 -s 31688
-    -t <timeframe> Timeframe to download: 1, hourly; 2, daily; 3, monthly [default: 2]
-    -y <years>     Years to download, express multiple years as a range: e.g. 1998:2008
-                   A single year can also be passed: e.g. 1998 (does not apply to monthly data)
-    -m <months>    Months to download, expressed as a range: e.g. 1:12
-                   If no month is given, 1:12 will be used. (only applies to hourly data)
-    -g             Optional; if specified, the individual CSV files will be "glued" into a single file
+Search Options
+  --name <name>        Filter stations by name, can use incomplete words, e.g. Tor
+  --prov <province>... Two letter code of the province (repeat as needed)
+  --period <period>    The period for which you need data, separated by a colon, e.g. 1981:2010
+  --type <type>        The type of data you are searching for (required if period is used)
+                       Options: 1, hourly; 2, daily; 3, monthly
+  --target <y> [<x>]   Either a station code, or space-separated longitude and latitude values
+  --dist <distance>    Colon-separated minimum and maximum distance from target [default: 0:100]
+  --recodes            Pass this flag for the program to suggest stations that may be combined to
+                       cover the period that you requested.
+
+Downloading Options:
+  -s <station>         Station code to download. Pass the argument multiple times for more than one
+                       station: e.g. -s 5051 -s 31688
+  -t <timeframe>       Timeframe to download: 1, hourly; 2, daily; 3, monthly [default: 2]
+  -y <years>           Years to download, express multiple years as a range: e.g. 1998:2008
+                       A single year can also be passed: e.g. 1998 (does not apply to monthly data)
+  -m <months>          Months to download, expressed as a range: e.g. 1:12
+                       If no month is given, 1:12 will be used. (only applies to hourly data)
 
 Other options:
-    -h --help      Show this help text
-    -v --version   Print the program version and exit
+  --outfile <filename> Save your search results to a csv file or override the name for the
+                       downloaded data.
+  -h --help            Show this help text
+  --version            Print the program version and exit
 
 Examples:
-  ec3 -i # downloads the data inventory csv.
-  ec3 -s 5051 -y 1981:2010 -g # creates a single daily .csv file for Toronto daily data
-  ec3 -s 5051 -y 1981:2010 -m 6:8 -t 1  # downloads hourly data for the summer months from 1981 to 2010 at Toronto
+  ec3 inv # downloads the data inventory csv.
+  ec3 search -n Toronto # find stations with "Toronto" in their name
+  ec3 get -s 5051 -y 1981:2010 # creates a single daily .csv file for Toronto daily data
+  ec3 get -s 5051 -y 1981:2010 -m 6:8 -t 1 # downloads hourly data for the summer months from 1981 to 2010 at Toronto
 """
 
 from docopt import docopt
 import re
 import os
-import sys
+from sys import exit
 import urllib.request
-import time
+import pandas as pd
+from time import sleep
+from geopy.distance import distance
+from operator import itemgetter
+from warnings import warn
+from tempfile import mkdtemp
+
 
 def download_file(url, filename):
+    print("Downloading", os.path.basename(filename), "to",
+          os.path.dirname(filename) if os.path.dirname(filename) != '' else "current working directory")
     try:
         resp = urllib.request.urlretrieve(url, filename)
     except urllib.error.URLError as e:
-        sys.exit("There was an error finding that file! The error was: {}".format(e))
+        exit("There was an error finding that file! The error was: {}".format(e))
     except urllib.error.HTTPError as e:
-        sys.exit("There was an error downloading that file! The error was: {}".format(e))
+        exit("There was an error downloading that file! The error was: {}".format(e))
 
-if __name__ == '__main__':
-    arguments = docopt(__doc__, version='ec3 2.0')
 
-    if arguments['-i']:
-        print("Downloading station data inventory.")
-        download_file("ftp://client_climate@ftp.tor.ec.gc.ca/Pub/Get_More_Data_Plus_de_donnees/Station%20Inventory%20EN.csv", "Station Inventory EN.csv")
-        sys.exit(0)
+def guess_skip(filename):
+    with (open(filename, 'r')) as file:
+        lines = file.read().splitlines()
+    return lines.index(max(lines, key = len))
+
+
+def station_search(name=None, province=None, period=None, type=None, detect_recodes=False, target=None, dist=range(101)):
+
+    filename = "Station Inventory EN.csv"
+    if not os.path.isfile(filename):
+        exit("Cannot find the station inventory. Download it with 'ec3 inv'")
+
+    inv = pd.read_csv(filename, skiprows = guess_skip(filename))
+    # Correct some placeholder coordinates (list comp because otherwise I get warnings)
+    inv['Latitude (Decimal Degrees)'] = [i if i != 40 else '' for i in inv['Latitude (Decimal Degrees)']]
+    inv['Longitude (Decimal Degrees)'] = [i if i != -50 else '' for i in inv['Longitude (Decimal Degrees)']]
+
+    filt = inv.copy()
+
+    if name is not None:
+        nmreg = re.compile(name, flags = re.IGNORECASE)
+        filt = filt[filt.Name.isin(list(filter(nmreg.match, filt.Name)))]
+
+        if filt.shape[0] == 0:
+            print("No results!")
+            return
+
+    if province is not None:
+        p_pass = [i.upper() for i in province]
+        p_codes = ["AB", "BC", "MB", "NB", "NL", "NT", "NS", "NU", "ON", "PE",
+                    "QC", "SK", "YT"]
+        if all([len(i) == 2 for i in p_pass]):
+            if not all([i.upper() in p_codes for i in province]):
+                exit("Incorrect province code(s) provided.")
+            indeces = [i for i, x in enumerate([i in p_pass for i in p_codes]) if x]
+            pull_prov = itemgetter(*indeces)(sorted(list(set(inv.Province))))
+            if isinstance(pull_prov, str):
+                pull_prov = [pull_prov]
+
+            filt = filt[filt.Province.isin(pull_prov)]
+
+        if filt.shape[0] == 0:
+            print("No results!")
+            return
+
+    # Next, set the data we are interested in, if necessary
+    if period is not None:
+        if type is None:
+            warnings.warn("No data type passed. Ignoring data filter.")
+        else:
+            if type in ['1', 'h', 'H']:
+                wantcols = ['HLY First Year', 'HLY Last Year']
+                dropcols = ['DLY First Year', 'DLY Last Year', 'MLY First Year', 'MLY Last Year']
+            elif type in ['2', "d", 'D']:
+                wantcols = ['DLY First Year', 'DLY Last Year']
+                dropcols = ['HLY First Year', 'HLY Last Year', 'MLY First Year', 'MLY Last Year']
+            elif type in ['3', 'm', 'M']:
+                wantcols = ['MLY First Year', 'MLY Last Year']
+                dropcols = ['HLY First Year', 'HLY Last Year', 'DLY First Year', 'DLY Last Year']
+            else:
+                period = None
+                warnings.warn("Invalid data type passed. Ignoring data filter.")
+
+    if target is not None:
+        if len(target) == 1:
+            coords = inv[['Latitude (Decimal Degrees)', 'Longitude (Decimal Degrees)']][inv['Station ID'] == target]
+            p1 = tuple(coords.values[0])
+        elif len(target) == 2:
+            p1 = target
+
+        filt = filt.assign(Dist = filt[['Latitude (Decimal Degrees)', 'Longitude (Decimal Degrees)']].apply(lambda x: distance(tuple(x), p1), axis = 1))
+        filt = filt[(filt.Dist <= max(dist)) & (filt.Dist >= min(dist))]
+
+        if filt.shape[0] == 0:
+            print("No results!")
+            return
+
+    if period is not None:
+        filt = filt.drop(dropcols, axis=1)
+        inside = filt[(filt[wantcols[0]] <= min(period)) & (filt[wantcols[1]] >= max(period))]
+        outside = filt[~filt['Station ID'].isin(inside['Station ID'])]
+        filt = inside
+
+        if detect_recodes:
+            # Try to detected cases where the StationID has changed
+            coords = outside[['Station ID', 'Latitude (Decimal Degrees)', 'Longitude (Decimal Degrees)']].groupby(['Latitude (Decimal Degrees)', 'Longitude (Decimal Degrees)']).count().reset_index()
+            coords = coords[coords['Station ID'] > 1]
+            printed = False
+            for rw in coords.index:
+                dups = outside[(outside['Latitude (Decimal Degrees)'] == coords['Latitude (Decimal Degrees)'][rw]) &
+                               (outside['Longitude (Decimal Degrees)'] == coords['Longitude (Decimal Degrees)'][rw])]
+                if (~dups[wantcols[0]].isnull().any()) & (min(dups[wantcols[0]]) <= min(period)):
+                    if (~dups[wantcols[1]].isnull().any()) & (max(dups[wantcols[1]]) >= max(period)):
+                        if min(dups[wantcols[1]]) <= max(dups[wantcols[0]]):
+                            if not printed:
+                                print("Note: In addition to the stations found, the following\n", \
+                                      "combinations may provide sufficient baseline data.\n\n")
+                                printed = True
+                                combo = 1
+                            print(">> Combination", combo, "at coordinates", coords['Latitude (Decimal Degrees)'][rw], \
+                                  coords['Longitude (Decimal Degrees)'][rw], "\n\n")
+                            for r in range(dups.shape[0]):
+                                print("Station {}: {}".format(dups.iloc[[r]]['Station ID'].values[0], dups.iloc[[r]].Name.values[0]))
+                            combo += combo
+
+        if filt.shape[0] == 0:
+            print("No results!")
+            return
+
+    return filt
+
+
+def get_data(arguments):
+    tempdir = mkdtemp()
 
     if arguments['-t'] is None:
         timeframe = 2
@@ -61,7 +195,7 @@ if __name__ == '__main__':
         elif arguments['-t'][0] in ['3', 'm', 'M']:
             timeframe = 3
         else:
-            sys.exit("Invalid timeframe passed.")
+            exit("Invalid timeframe passed.")
 
     if arguments['-m'] is None:
         if timeframe == 1:
@@ -74,17 +208,17 @@ if __name__ == '__main__':
             months = [6]
         else:
             if not bool(re.search(r'(^[0-9]{1,2}$|^[0-9]{1,2}:[0-9]{1,2}$)', arguments['-m'])):
-                sys.exit("Invalid month format.")
+                exit("Invalid month format.")
             else:
                 months = [int(x) for x in arguments['-m'].split(":")]
                 if any(x not in range(13) for x in months):
-                    sys.exit("Months must be from 1 to 12.")
+                    exit("Months must be from 1 to 12.")
                 else:
                     months = range(min(months), max(months) + 1)
 
     if arguments['-y'] is None:
         if timeframe != 3:
-            sys.exit("Years must be specified!")
+            exit("Years must be specified!")
         else:
             years = [1989]
     else:
@@ -93,19 +227,12 @@ if __name__ == '__main__':
             years = [1989]
         else:
             if not bool(re.search(r'(^[0-9]{4}$|^[0-9]{4}:[0-9]{4}$)', arguments['-y'])):
-                sys.exit("Invalid year format.")
+                exit("Invalid year format.")
             else:
                 years = [int(x) for x in arguments['-y'].split(":")]
                 years = range(min(years), max(years) + 1)
 
     for station in arguments['-s']:
-
-        if arguments['-g']:
-            first_file = True
-            guess_skip = True
-        else:
-            first_file = False
-            guess_skip = False
 
         for year in years:
             for month in months:
@@ -123,32 +250,75 @@ if __name__ == '__main__':
                     fileyr=""
 
                 url = "http://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID={}&Year={}&Month={}&Day=14&timeframe={}&submit=Download+Data".format(station, year, month, timeframe)
-
-                filename="{}-{}{}{}.csv".format(station, period, fileyr, filemth)
-
-                print("Downloading", filename)
+                filename = os.path.join(tempdir, "{}-{}{}{}.csv".format(station, period, fileyr, filemth))
                 download_file(url, filename)
 
-                if arguments['-g']:
-                    glued_filename = "{}-{}.csv".format(station, period)
+                try:
+                    dat
+                except NameError:
+                    dat = pd.read_csv(filename, skiprows = guess_skip(filename))
+                else:
+                    dat = dat.append(pd.read_csv(filename, skiprows = guess_skip(filename)))
 
-                    with (open(filename, 'r')) as file:
-                        lines = file.read().splitlines()
+                sleep(0.5)
 
-                    if guess_skip:
-                        pull = lines.index(max(lines, key = len))
-                        if not first_file:
-                            pull += 1
+        if arguments['--outfile'] is not None:
+            outfile = arguments['--outfile']
+        else:
+            outfile = "{}-{}.csv".format(station, period)
+        print("Saving data to", outfile)
+        dat.to_csv(outfile)
 
-                    if first_file:
-                        with open(glued_filename, 'w') as file:
-                            file.write("\n".join(lines[pull:]))
-                            pull += 1
-                            first_file = False
-                    else:
-                        with open(glued_filename, 'a') as file:
-                            file.write("\n" + "\n".join(lines[pull:]))
 
-                    os.remove(filename)
+if __name__ == '__main__':
+    arguments = docopt(__doc__, version='ec3 2.0')
 
-                time.sleep(0.5)
+    if arguments['find']:
+        if arguments['--period'] is not None:
+            if not bool(re.search(r'(^[0-9]{4}$|^[0-9]{4}:[0-9]{4}$)', arguments['--period'])):
+                warnings.warn("Invalid period format.")
+                period = None
+            else:
+                period = [int(x) for x in arguments['--period'].split(":")]
+        else:
+            period = None
+
+        if arguments['--target'] is not None:
+            target = arguments['--target']
+            if arguments['<x>'] is not None:
+                target = (target, arguments['<x>'])
+        else:
+            target = None
+
+        if arguments['--dist'] is not None:
+            if not bool(re.search(r'(^[0-9]{1,4}:[0-9]{1,4}$)', arguments['--dist'])):
+                warnings.warn("Invalid dist format.")
+                dist = range(101)
+            else:
+                dist = [int(x) for x in arguments['--dist'].split(":")]
+                dist = range(min(dist), max(dist) + 1)
+        else:
+            dist = None
+
+        province = None if len(arguments['--prov']) == 0 else arguments['--prov']
+
+        results = station_search(name=arguments['--name'], province=province,
+                                 period=period, type=arguments['--type'],
+                                 detect_recodes=arguments['--recodes'],
+                                 target=target, dist=dist)
+
+        if results is not None:
+            if arguments['--outfile'] is not None:
+                results.to_csv(arguments['--outfile'])
+            print(results)
+
+        exit(0)
+
+    if arguments['inv']:
+        print("Downloading station data inventory.")
+        download_file("ftp://client_climate@ftp.tor.ec.gc.ca/Pub/Get_More_Data_Plus_de_donnees/Station%20Inventory%20EN.csv", "Station Inventory EN.csv")
+        exit(0)
+
+    if arguments['get']:
+        get_data(arguments)
+        exit(0)
