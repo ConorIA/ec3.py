86004b5bd128570744884396a90fcbe1e57f7f82
Conor Anderson <conor@conr.ca>
2019-01-26 23:25:11 -0500

    Add pandas.hook for fix Windows binary

---
+++ .misc/hooks/hook-pandas.py
@@ -0,0 +1,4 @@
+hiddenimports = ['pandas._libs.tslibs.timedeltas',
+                 'pandas._libs.tslibs.nattype',
+                 'pandas._libs.tslibs.np_datetime',
+                 'pandas._libs.skiplist']
