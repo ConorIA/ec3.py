bd4723cd59e4aa5b211bf70a94c90620ca2360c9
Conor Anderson <conor@conr.ca>
2021-10-19 07:25:49 -0400

    Update pyinstaller spec

--- .misc/hooks/hook-pandas.py
+++
@@ -1,4 +0,0 @@
-hiddenimports = ['pandas._libs.tslibs.timedeltas',
-                 'pandas._libs.tslibs.nattype',
-                 'pandas._libs.tslibs.np_datetime',
-                 'pandas._libs.skiplist']
